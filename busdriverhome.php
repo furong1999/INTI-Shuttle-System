00aacffe17831405499379b964b37efe3a165fcd
yukkie97 <53140292+yukkie97@users.noreply.github.com>
2020-07-12 13:56:17 +0800

    bus driver homepage

---
+++ busdriverhome.php
@@ -0,0 +1,323 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>iPortfolio Bootstrap Template - Index</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="iPortfolio/assets/img/favicon.png" rel="icon">
+  <link href="iPortfolio/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="iPortfolio/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="iPortfolio/assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="iPortfolio/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="iPortfolio/assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="iPortfolio/assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="iPortfolio/assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="iPortfolio/assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: iPortfolio - v1.3.0
+  * Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Mobile nav toggle button ======= -->
+  <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="d-flex flex-column">
+
+      <div class="profile">
+        <img src="iPortfolio/assets/img/profile-img.jpg" alt="" class="img-fluid rounded-circle">
+        <h1 class="text-light"><a href="busdriverhome.php">Tan Eng Heng</a></h1>
+        <div class="social-links mt-3 text-center">
+          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+      </div>
+
+      <nav class="nav-menu">
+        <ul>
+          <li class="active"><a href="#hero"><i class="bx bx-home"></i> <span>Home</span></a></li>
+          <li><a href="#about"><i class="bx bx-user"></i> <span>About Me</span></a></li>
+          <li><a href="#contact"><i class="bx bx-file-blank"></i> <span>Contact Me</span></a></li>
+          <li><a href="#route"><i class="bx bx-book-content"></i> Routes</a></li>
+          <li><a href="#editprofile"><i class="bx bx-envelope"></i> Edit Profile</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+      <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="hero-container" data-aos="fade-in">
+      <h1>Mr Tan</h1>
+      <p>I'm <span class="typed" data-typed-items="Bus Driver"></span></p>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>About</h2>
+          <p></p>
+      </div>
+
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right">
+            <img src="iPortfolio/assets/img/profile-img.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>INTI SHUTTLE BUS DRIVER.</h3>
+            <p class="font-italic">
+              A bus driver with 4 years working experiences. Hope to sent my customers to theirs destination safe and on time.
+            </p>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="icofont-rounded-right"></i> <strong>Name:</strong> Tan Eng Heng</li>
+                  <li><i class="icofont-rounded-right"></i> <strong>Working Days:</strong> Monday - Fridays </li>
+                  <li><i class="icofont-rounded-right"></i> <strong>Working Hours: </strong> 8.00am - 6.00pm</li>
+                  <li><i class="icofont-rounded-right"></i> <strong>City:</strong> Penang, Malaysia</li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="icofont-rounded-right"></i> <strong>Age:</strong> 30</li>
+                  <li><i class="icofont-rounded-right"></i> <strong>Working Experiences: </strong> 4 Years</li>
+                  <li><i class="icofont-rounded-right"></i> <strong>Bus Number:</strong> PLH 3023</li>
+
+                </ul>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Contact Me</h2>
+        </div>
+
+        <div class="row" data-aos="fade-in">
+
+          <div class="col-lg-5 d-flex align-items-stretch">
+            <div class="info">
+              <div class="address">
+                <i class="icofont-google-map"></i>
+                <h4>Location:</h4>
+                <p>1-Z, Lebuh Bukit Jambul, Bukit Jambul, 11900 Bayan Lepas, Pulau Pinang</p>
+              </div>
+
+              <div class="email">
+                <i class="icofont-envelope"></i>
+                <h4>Email:</h4>
+                <p>Tanengheng@intishuttle.my</p>
+              </div>
+
+              <div class="phone">
+                <i class="icofont-phone"></i>
+                <h4>Call:</h4>
+                <p>0147089221</p>
+              </div>
+
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3972.4919887247825!2d100.2796820147649!3d5.341603796125226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x304ac048a161f277%3A0x881c46d428b3162c!2sINTI%20International%20College%20Penang!5e0!3m2!1sen!2smy!4v1594458772042!5m2!1sen!2smy" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+            </div>
+
+          </div>
+
+          <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+            <form action="iPortfolio/forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="form-row">
+                <div class="form-group col-md-6">
+                  <label for="name">Your Name</label>
+                  <input type="text" name="name" class="form-control" id="name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                  <div class="validate"></div>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Your Email</label>
+                  <input type="email" class="form-control" name="email" id="email" data-rule="email" data-msg="Please enter a valid email" />
+                  <div class="validate"></div>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="name">Subject</label>
+                <input type="text" class="form-control" name="subject" id="subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                <div class="validate"></div>
+              </div>
+              <div class="form-group">
+                <label for="name">Message</label>
+                <textarea class="form-control" name="message" rows="10" data-rule="required" data-msg="Please write something for us"></textarea>
+                <div class="validate"></div>
+              </div>
+              <div class="mb-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+
+    <!-- ======= Portfolio Section ======= -->
+    <section id="route" class="portfolio section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Routes</h2>
+          <p> My daily Routes</p>
+        </div>
+
+        <div class="row" data-aos="fade-up">
+          <div class="col-lg-12 d-flex justify-content-center">
+            <ul id="portfolio-flters">
+              <li data-filter="*" class="filter-active">All</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="100">
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="Map/map1.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="Map/map1.jpg" data-gall="portfolioGallery" class="venobox" title="Routes 1"><i class="bx bx-plus"></i></a>
+                <a href="busdriverhomedetails.php" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+              <img src="Map/map2.jpg" class="img-fluid" alt="">
+              <div class="portfolio-links">
+                <a href="Map/map2.jpg" data-gall="portfolioGallery" class="venobox" title="Route 2"><i class="bx bx-plus"></i></a>
+                <a href="busdriverhomedetails2.php" title="More Details"><i class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
+
+      <section id="editprofile" class="portfolio section-bg">
+          <div class="container">
+
+              <div class="section-title">
+                  <h2>Edit Profile</h2>
+              </div>
+
+
+              <div align="center">
+                  <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+
+                      <form action="iPortfolio/forms/contact.php" method="post" role="form" class="php-email-form">
+                          <div class="form-group">
+                              <label for="name">Your Name</label>
+                              <input type="text" name="name" class="form-control" id="name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                              <div class="validate"></div>
+                          </div>
+
+                          <div class="form-group">
+                              <label for="name">Your Email</label>
+                              <input type="email" class="form-control" name="email" id="email" data-rule="email" data-msg="Please enter a valid email" />
+                              <div class="validate"></div>
+                          </div>
+
+                          <div class="form-group">
+                              <label for="name">Your Contact Number</label>
+                              <input type="text" class="form-control" name="contact" id="contact" data-rule="minlen:10 " data-msg="Please enter at least 8 chars of subject" />
+                              <div class="validate"></div>
+                          </div>
+
+                          <div class="text-center"><button type="submit">Edit Profile</button></div>
+                      </form>
+                  </div>
+              </div>
+          </div>
+          </div>
+      </section>
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>iPortfolio</span></strong>
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="iPortfolio/assets/vendor/jquery/jquery.min.js"></script>
+  <script src="iPortfolio/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="iPortfolio/assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="iPortfolio/assets/vendor/php-email-form/validate.js"></script>
+  <script src="iPortfolio/assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="iPortfolio/assets/vendor/counterup/counterup.min.js"></script>
+  <script src="iPortfolio/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="iPortfolio/assets/vendor/venobox/venobox.min.js"></script>
+  <script src="iPortfolio/assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="iPortfolio/assets/vendor/typed.js/typed.min.js"></script>
+  <script src="iPortfolio/assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="iPortfolio/assets/js/main.js"></script>
+
+</body>
+
+</html>
